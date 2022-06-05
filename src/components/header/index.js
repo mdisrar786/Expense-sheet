e08a22399777e62b2a9020ccfe83e483a5edda3b
israr <mdisrar164@gmail.com>
2022-06-06 00:00:54 +0530

    add git Account

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -10,7 +10,7 @@ const Header = () => {
         </div>
         <div className="header-button">
           <a
-            href="https://github.com/shuvamk"
+            href="https://github.com/mdisrar786"
             target="_blank"
             rel="noopener noreferrer"
           >
